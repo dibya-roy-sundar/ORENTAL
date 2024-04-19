40ca49f05d98cfbf23debb34557efba5d7b7b827
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2024-04-20 02:06:23 +0530

    user auth done

---
+++ server/models/user.js
@@ -0,0 +1,44 @@
+const  mongoose = require('mongoose');
+const  jwt = require('jsonwebtoken');
+const  bcrypt = require('bcrypt');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is Required"],
+    },
+    //use JOI for further validation
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        select: false
+    },
+    phnNo: {
+        type: Number,
+        required: [true, "Phone no is required"],
+        unique: true,
+    }
+});
+
+userSchema.methods.getJWTToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE,
+    });
+}
+
+userSchema.statics.findAndValidate = async function (email, password) {
+    const foundUser = await this.findOne({ email }).select("+password");
+    //if a user is found, this means that the username is already in use
+    if (!foundUser) return false;
+
+    //if username is unique, then we will verify the password
+    const isValid = await bcrypt.compare(password, foundUser.password);
+    return isValid ? foundUser : false;
+}
+
+module.exports = mongoose.model('User', userSchema);
